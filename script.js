f956383e3d950284caf01820a2d7bdeef5071b1f
Josh Baker <josh.jacob.baker@gmail.com>
2021-11-01 15:33:24 -0700

    ticket-4 added callback function for cocoSsd model to ensure webpage waits for the model to be completely loaded in before allowing user to open webcam

--- script.js
+++ script.js
@@ -55,9 +55,17 @@ function enableCam(event) {
     video.addEventListener("loadeddata", predictWebcam)
   })
 }
-// Placeholder function for next step.
-function predictWebcam() {}
 
-// Pretend model has loaded so we can try out the webcam code.
-var model = true
-demosSection.classList.remove("invisible")
+// Store the resulting model in the global scope of our app.
+var model = undefined
+
+// Before we can use COCO-SSD class we must wait for it to finish
+// loading. Machine Learning models can be large and take a moment
+// to get everything needed to run.
+// Note: cocoSsd is an external object loaded from our index.html
+// script tag import so ignore any warning in Glitch.
+cocoSsd.load().then(function (loadedModel) {
+  model = loadedModel
+  // Show demo section now model is ready to use.
+  demosSection.classList.remove("invisible")
+})
